d03ea92a5827f4ad0e31012f99d499906de28aa9
CrissN5045 <cherrera@isppro.net>
2023-01-05 12:00:50 -0600

    sitio actulizado

---
+++ public/conf.php
@@ -0,0 +1,11 @@
+<?php
+
+define("SITE_NAME","Central de Apuesta");
+define("DOMAIN_NAME","centraldeapuesta.com");
+define("DGS_SITEID","604");
+
+require "C:\\inetpub\\wwwroot\\generalConfig.php";
+
+?>
+
+
